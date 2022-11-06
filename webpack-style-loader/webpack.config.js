d57f8061c563ef2dccd09b29b13919e1837898de
a-sabagh <a.sabagh72@gmail.com>
2022-11-06 14:10:37 +0330

    style-loader css-loader

--- webpack-style-loader/webpack.config.js
+++ webpack-style-loader/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path')
+
 const configuration = {
 	entry: './src/index.js',
 	output: {
 		filename: 'build.js',
 		path: path.resolve(__dirname,'build'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ['style-loader','css-loader']
+			}
+		]
 	},
 	mode: "development"
 }
