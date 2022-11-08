b3c1b49e8b5d2c9ca0dd4cc9a988d76c4cf6f2d4
a-sabagh <a.sabagh72@gmail.com>
2022-11-09 01:03:14 +0330

    html webpack hash option

---
+++ webpack-html-webpack-plugin/part4/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const config = {
+	mode: 'development',
+	entry: {
+		index: './src/assets/js/index.js',
+		about: './src/assets/js/about.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname,'dist')
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'index',
+			filename: 'index.html',
+			chunks: 'index',
+			hash: true,
+		}),
+		new HtmlWebpackPlugin({
+			title: 'about',
+			filename: 'about.html',
+			chunks: 'about',
+			hash: true,
+		})
+	]
+}
+
+module.exports = config
