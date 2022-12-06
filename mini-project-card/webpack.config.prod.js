11196f39dfcde5376ebed966bdc42c397c82bb20
a-sabagh <a.sabagh72@gmail.com>
2022-12-06 23:52:47 +0330

    change development and production mode structure

--- mini-project-card/webpack.config.prod.js
+++ mini-project-card/webpack.config.prod.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 const config = {
 	mode: "development",
+	devtool: 'source-map',
 	entry: {
 		index: './src/index.js',
 	},
