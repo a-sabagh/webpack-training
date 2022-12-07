fd23441c686db72d0e10a4bc3428b66ef2dcbf67
a-sabagh <a.sabagh72@gmail.com>
2022-12-08 02:19:51 +0330

    dont repeat yourself by merge function

---
+++ mini-project-card/webpack.config.common.js
@@ -0,0 +1,69 @@
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+const config = {
+	entry: {
+		index: './src/index.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname,'./build'),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: ["@babel/preset-env"]
+						}
+					}
+				]
+			},
+			{
+				test: /\.s?css$/,
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							url: {},
+						}
+					},
+					'sass-loader',
+				]
+			},
+			{
+				test: /\.(woff|eot|ttf|svg|jpg|png|gif)$/i,
+				use: [
+					 {
+						loader: 'url-loader',
+						options: {}
+					 }
+				]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'assets/css/[name].css',
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html',
+		}),
+		new HtmlWebpackPlugin({
+			filename: 'about.html',
+			template: './src/about.html',
+		})
+	]
+}
+
+module.exports = config
